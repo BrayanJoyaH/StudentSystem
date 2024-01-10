81ac73fd0b0f1d33845f46c625d3a5500c00b59a
Brayan Camilo Joya Herrea <b.joya@uniandes.edu.co>
2024-01-10 10:59:46 -0500

    Versión Funcional V1.0.1

--- index.php
+++ index.php
@@ -9,16 +9,15 @@
 		<script src="https://kit.fontawesome.com/776212aa00.js" crossorigin="anonymous"></script>
 	</head>
 	<body>
-		<form method="POST" action="Usuarios/Controladores/login.php">
-			<h1>Inicio de Sesión</h1>
-			Usuario <br>
-			<input type="text" name="Usuario" required="" autocomplete="off" placeholder="Usuario"><br><br>
-			Contraseña <br>
-			<input type="password" name="Contrasena" required="" autocomplete="off" placeholder="Contraseña"> <br><br>
-			<input type="submit" value="Inicia Sesión">
-		</form>
-		<footer>
-			<h3>©Copyright - Todos los derechos reservados Elipson WBJ</h3>
-		</footer>
+		<main>
+			<form method="POST" action="Usuarios/Controladores/login.php">
+				<h1>Inicio de Sesión</h1>
+				Usuario <br>
+				<input type="text" name="Usuario" required="" autocomplete="off" placeholder="Usuario"><br><br>
+				Contraseña <br>
+				<input type="password" name="Contrasena" required="" autocomplete="off" placeholder="Contraseña"> <br><br>
+				<input type="submit" value="Inicia Sesión">
+			</form>
+		</main>
 	</body>
 </html>
--- school.sql
+++ school.sql
@@ -1,30 +1,138 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 10, 2024 at 04:57 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
 
-CREATE TABLE usuarios
-(
-	ID_USUARIO INT(15) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	NOMBRE VARCHAR(60) NOT NULL,
-	APELLIDO VARCHAR(60) NOT NULL,
-	USUARIO VARCHAR(40) NOT NULL,
-	PASSWORD VARCHAR(80) NOT NULL,
-	PERFIL VARCHAR(30) NOT NULL,
-	ESTADO VARCHAR(20) NOT NULL
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE materia
-(
-	ID_MATERIA INT(15) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	MATERIA VARCHAR(30) NOT NULL
-);
 
-CREATE TABLE estudiantes
-(
-	ID_ESTUDIANTES INT(15) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	NOMBRE VARCHAR(60) NOT NULL,
-	APELLIDO VARCHAR(60) NOT NULL,
-	DOCUMENTO VARCHAR(12) NOT NULL,
-	CORREO VARCHAR(60) NOT NULL,
-	MATERIA VARCHAR(30) NOT NULL,
-	DOCENTE VARCHAR(60) NOT NULL,
-	PROMEDIO INT(3) NOT NULL,
-	FECHA_REGISTRO DATE NOT NULL
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `school`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `estudiantes`
+--
+
+CREATE TABLE `estudiantes` (
+  `ID_ESTUDIANTES` int(15) NOT NULL,
+  `NOMBRE` varchar(60) NOT NULL,
+  `APELLIDO` varchar(60) NOT NULL,
+  `DOCUMENTO` varchar(12) NOT NULL,
+  `CORREO` varchar(60) NOT NULL,
+  `MATERIA` varchar(30) NOT NULL,
+  `DOCENTE` varchar(60) NOT NULL,
+  `PROMEDIO` int(3) NOT NULL,
+  `FECHA_REGISTRO` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `estudiantes`
+--
+
+INSERT INTO `estudiantes` (`ID_ESTUDIANTES`, `NOMBRE`, `APELLIDO`, `DOCUMENTO`, `CORREO`, `MATERIA`, `DOCENTE`, `PROMEDIO`, `FECHA_REGISTRO`) VALUES
+(2, 'Brayan', 'Joya', '1074158246', 'brayanjoya@example.org', 'Matemáticas', 'Dioselina López', 4, '2024-01-10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `materia`
+--
+
+CREATE TABLE `materia` (
+  `ID_MATERIA` int(15) NOT NULL,
+  `MATERIA` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `materia`
+--
+
+INSERT INTO `materia` (`ID_MATERIA`, `MATERIA`) VALUES
+(1, 'Matemáticas');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `ID_USUARIO` int(15) NOT NULL,
+  `NOMBRE` varchar(60) NOT NULL,
+  `APELLIDO` varchar(60) NOT NULL,
+  `USUARIO` varchar(40) NOT NULL,
+  `PASSWORD` varchar(80) NOT NULL,
+  `PERFIL` varchar(30) NOT NULL,
+  `ESTADO` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+INSERT INTO `usuarios` (`ID_USUARIO`, `NOMBRE`, `APELLIDO`, `USUARIO`, `PASSWORD`, `PERFIL`, `ESTADO`) VALUES
+(1, 'Brayan', 'Joya', 'brayanjoya', 'admin', 'Administrador', '1'),
+(2, 'Dioselina', 'López', 'Dioselina', '12345678', 'Docente', 'Activo');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `estudiantes`
+--
+ALTER TABLE `estudiantes`
+  ADD PRIMARY KEY (`ID_ESTUDIANTES`);
+
+--
+-- Indexes for table `materia`
+--
+ALTER TABLE `materia`
+  ADD PRIMARY KEY (`ID_MATERIA`);
+
+--
+-- Indexes for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`ID_USUARIO`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `estudiantes`
+--
+ALTER TABLE `estudiantes`
+  MODIFY `ID_ESTUDIANTES` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `materia`
+--
+ALTER TABLE `materia`
+  MODIFY `ID_MATERIA` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `ID_USUARIO` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
